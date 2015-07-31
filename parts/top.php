2f9c4b681f6efa6f82300c8f133ea8b085dac85e
Derrick Egersdorfer <derrick@egersdorfer.co.za>
2015-07-31 08:23:42 +0200

    TopE block fix

--- parts/top.php
+++ parts/top.php
@@ -6,7 +6,7 @@
         $show = true;
 
         // Find the correct word count
-        $count = count(array_filter(array($topA, $topB, $topC, $topD, $topD)));
+        $count = count(array_filter(array($topA, $topB, $topC, $topD, $topE)));
         switch( $count ){
             case 1 : $total = "one"; break;
             case 2 : $total = "two"; break;
