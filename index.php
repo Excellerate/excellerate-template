8325c27295efd2d0fdfabb29b2e76967ef3275c2
Derrick Egersdorfer <derrick@egersdorfer.co.za>
2015-08-20 17:32:26 +0200

    Logo fix

--- index.php
+++ index.php
@@ -97,7 +97,7 @@
 
         <!-- Icons -->
         <link rel="shortcut icon" href="<?= $template; ?>/assets/img/groupLogos/<?= $logo; ?>.ico" type="image/x-icon">
-        <link rel="icon" href="<?= $template; ?>/assets/img/groupLogos/<?= $logo; ?>.ico" type="image/x-icon">
+        <link rel="icon" href="<?= $template; ?>/assets/img/groupLogos/<?= $logoA; ?>.ico" type="image/x-icon">
 
         <!-- Excellerate Font -->
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
