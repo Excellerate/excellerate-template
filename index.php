59c23f89d7728d68df6f0f12598ac43b26ff7662
Derrick Egersdorfer <derrick@egersdorfer.co.za>
2015-09-07 05:42:42 +0200

    Small changes

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 
     // Set logo width and height
     if($logoA and $logoB and $logoC){
-        $logoWidth = "width=150px";
+        $logoWidth = "width=151px";
     }
     else if ($logoA and $logoB){
         $logoWidth = "width=226px";
@@ -97,6 +97,10 @@
 
         <!-- Joomla Head -->
         <jdoc:include type="head" />
+
+        <!--[if IE]>
+        <link rel="stylesheet" type="text/css" href="<?= $template; ?>/assets/css/ie.css" />
+        <![endif]-->
 
         <!-- Icons -->
         <link rel="shortcut icon" href="<?= $template; ?>/assets/img/groupLogos/<?= $logoA; ?>.ico" type="image/x-icon">
