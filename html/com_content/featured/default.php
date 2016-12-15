6dec429c08599bb580e09c84d3e4bb3624c19bff
Derrick Egersdorfer <derrick@egersdorfer.co.za>
2016-12-15 11:22:33 +0200

    Front page now supports featured articles

--- html/com_content/featured/default.php
+++ html/com_content/featured/default.php
@@ -1,5 +1,8 @@
 <?php include JPATH_BASE . '/templates/excellerate/helper.php'; ?>
 <div id="featured">
+
+<h2 class="ui dividing header">Featured Articles</h2>
+
 <?php foreach($this->items as $item) : ?>
 	<?php
 			$this->item = &$item;
