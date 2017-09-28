66982c1479304b2d7c7e707571e04eb054f8d437
CodeChap <hello@codechap.com>
2017-09-28 14:15:10 +0200

    Count cards row

--- parts/cards.php
+++ parts/cards.php
@@ -1,4 +1,29 @@
-<div class="ui three stackable cards">
+<?php
+    // Row A to E
+    if($cardA or $cardB or $cardC or $cardD or $cardE or $cardf){
+
+        // Create the row grid
+        $showAtoF = true;
+
+        // Find the correct word count
+        $count = count(array_filter(array($cardA, $cardB, $cardC, $cardD, $cardE, $cardF)));
+        switch( $count ){
+            case 1 : $use = "four"; break;
+            case 2 : $use = "four"; break;
+            case 3 : $use = "four"; break;
+            case 4 : $use = "four"; break;
+            case 5 : $use = "three"; break;
+            case 6 : $use = "three"; break;
+        }
+    }
+
+    else{
+        $showAtoF = false;
+    }
+
+?>
+
+<div class="ui <?= $use; ?> stackable cards">
   <jdoc:include type="modules" name="card_a" style="card" />
   <jdoc:include type="modules" name="card_b" style="card" />
   <jdoc:include type="modules" name="card_c" style="card" />
