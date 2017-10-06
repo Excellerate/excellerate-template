eef9445e71395ec48fa1aa5e4e3ddf03664b7f43
Derrick Egersdorfer <derrick@egersdorfer.co.za>
2017-10-06 09:25:42 +0200

    Cleanup

--- tmpl.php
+++ tmpl.php
@@ -89,7 +89,6 @@
   $doc = JFactory::getDocument();
   $doc->addStyleSheet(JUri::base() . 'templates/' . $this->template . '/assets/css/semantic.min.css', $type = 'text/css');
   $doc->addStyleSheet(JUri::base() . 'templates/' . $this->template . '/assets/css/layout.css', $type = 'text/css');
-  //$doc->addStyleSheet(JUri::base() . 'templates/' . $this->template . '/assets/css/styles/'.$style.'.css', $type = 'text/css');
   $doc->addScript($this->baseurl . '/templates/' . $this->template . '/assets/js/jquery.min.js', 'text/javascript');
   $doc->addScript($this->baseurl . '/templates/' . $this->template . '/assets/js/semantic.min.js', 'text/javascript');
 
