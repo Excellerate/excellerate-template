848ef099de99627a7fa6f81db41abb45b3424f54
CodeChap <hello@codechap.com>
2018-02-02 23:13:39 +0200

    SEO

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<meta charset="utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-		<meta name="Cache-Control" content="max-age=2592000, public">
+		<meta http-equiv="Expires" content="30">
 
 		<!-- Excellerate Font -->
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
