d318af300fa6adf7f3da7f3165ab997690dbbe7c
CodeChap <hello@codechap.com>
2018-02-02 23:24:13 +0200

    SEO

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<meta charset="utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-		<meta http-equiv="Expires" content="30">
+		<meta http-equiv="Expires" content="86400, private">
 
 		<!-- Excellerate Font -->
 		<style>
